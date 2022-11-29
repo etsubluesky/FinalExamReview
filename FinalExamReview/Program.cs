c4b6b08ad5df8372f50a49b7e94be61ee492fec0
desjardinsm-etsu <desjardinsm@etsu.edu>
2022-11-29 12:03:00 -0500

    Day 2 changes.

--- FinalExamReview/Program.cs
+++ FinalExamReview/Program.cs
@@ -6,88 +6,104 @@ namespace FinalExamReview
     {
         static void Main(string[] args)
         {
-            //primitive
-            int wholeNumber = 8;
-            //char letterOrANumber;
-            //bool trueOrFalse;
-            //double realNumbers;
-            //float anotherRealNumberMorePrecision;
-            //decimal anotherRealNumberMoreAccuracy;
+            int choice;
+            int choiceForNewSubMenu;
 
-            //long bigWholeNumber;
-            //byte smallWholeNumber;
-            //short smallWholeNumberButLargerThenAByteSmallerThenAnInt;
-
-            //ref data type
-            char[] letters = { 'a', 'b', 'c'};
-            char[] letters2 = { 'a', 'd', 'c' };
-            
-            
-            //model but it is also a ref data type
-            string name = "Matt";
-
-            //!= 
-            //==
-            //<
-            //>
-            //<=
-            //>=
-
-            if(letters.Length == letters2.Length)
-            {
-                bool isEqual = true;
-
-                //keep checking
-                for (int i = 0, y = 4; i < letters.Length; i++)
-                {
-                    if (letters[i] != letters2[i])
-                    {
-                        isEqual = false;
-                    }
-                }
-                
-                if(isEqual)
-                {
-                    Console.WriteLine("They are equal!");
-                }
-                else
-                {
-                    Console.WriteLine("They are not equal!");
-                }
-            }
-            else 
-            {
-                Console.WriteLine("They are not the same length");
-            }
-
-            switch(name)
-            {
-                case "Matt":
-                    //do something
-                    break;
-                case "Casey":
-                    //do something
-                    break;
-                default:
-                    //this will awlays run if none of the other case work
-                    break;
-            }
-
-            foreach (char item in letters)
-            {
-                Console.WriteLine(item);
-            }
-
-            while(true)
-            {
-                //repeat while true
-            }
-
+            //string[] courseArray = new string[5];
+            //List<string> courseList = new List<string>(5);
+            //courseList.
             do
             {
-                //do it at least one time
+                ClearScreen();
+                MainMenu();
+                Console.WriteLine("What options do you choose? ");
+                choice = Int32.Parse(Console.ReadLine());
+               
 
-            } while (true);
+                switch(choice)
+                {
+                    case 1:
+
+                        do
+                        {
+                            ClearScreen();
+                            NewSubMenu();
+                            Console.WriteLine("What options do you choose? ");
+                            choiceForNewSubMenu = Int32.Parse(Console.ReadLine());
+                        } while (choiceForNewSubMenu != 4);
+
+                        break;
+                    case 2:
+                        //Open Code
+                        
+                        //if(c)
+                        //{
+
+                        //}
+
+
+                        HoldScreen();
+                        break;
+                    case 3:
+                        //Save Code
+                        Console.WriteLine("We will implement this later!");
+                        HoldScreen();
+                        break;
+                }
+
+            } while (choice != 4);
+
+            Console.WriteLine("Thanks for using my program!");
         }
+
+        static void MainMenu()
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("Main Menu");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1. New");
+            Console.WriteLine("2. Array Stuff");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("--------------------");
+        }
+
+        static void NewSubMenu()
+        {
+            Console.WriteLine("--------------------");
+            Console.WriteLine("New Sub-Menu");
+            Console.WriteLine("--------------------");
+            Console.WriteLine("1. New Project");
+            Console.WriteLine("2. New Repo");
+            Console.WriteLine("3. New File");
+            Console.WriteLine("4. Go Back to Main Menu");
+            Console.WriteLine("--------------------");
+        }
+
+        static void ClearScreen() 
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("\n");
+            }
+        }
+
+        static void HoldScreen()
+        {
+            Console.WriteLine("Press Enter to Continue!");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        static void ArrayMenu()
+        {
+            Console.WriteLine("1. Add more space to an array.");
+            Console.WriteLine("2. Add something to the array.");
+            Console.WriteLine("3. Display array contents.");
+            Console.WriteLine("4. Delete something from the array.");
+            Console.WriteLine("5. Display a specific item of the array.");
+        }
+
+
     }
 }
